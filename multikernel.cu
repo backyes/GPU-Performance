2c5ea90987bc2950c7daebcd35dc845568738120
wangyanfei01 <wangyanfei01@baidu.com>
2016-09-22 22:02:09 +0800

    add multikernel.cu

---
+++ multikernel.cu
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <pthread.h>
+
+const int N = 1 << 27;
+
+__global__ void kernel(float *x, int n)
+{
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
+        x[i] = sqrt(pow(3.14159,i));
+    }
+}
+
+void *thread(void *args)
+{
+    int * thread_data = (int*) args;
+    const int num_streams = 8;
+
+    cudaStream_t streams[num_streams];
+    float *data[num_streams];
+
+    int deviceNum; 
+    cudaError_t ret = cudaGetDeviceCount(&deviceNum);
+    ret = cudaSetDevice(*thread_data);
+    printf("device num: %d\n", *thread_data);
+
+    for (int i = 0; i < num_streams; i++) {
+        cudaStreamCreate(&streams[i]);
+ 
+        cudaError_t  ret0 = cudaMalloc(&data[i], N * sizeof(float));
+
+        if (ret0 != cudaSuccess) {
+           printf("allocate failed\n");
+           return 0;
+        } else {
+           printf("%d MB\n", N*sizeof(float)/1024/1024);
+        }
+
+        // launch one worker kernel per stream
+        kernel<<<1, 64, 0, streams[i]>>>(data[i], N);
+
+        // launch a dummy kernel on the default stream
+        kernel<<<1, 1>>>(0, 0);
+
+        printf("finished stream syn %d\n", i);
+    }
+
+    printf("finished all stream %d\n");
+    cudaDeviceReset();
+    printf("finished device reset %d\n");
+
+    return 0;
+}
+
+#define THREAD_NUM 1
+
+int main() {
+  pthread_t threads[THREAD_NUM];
+
+  int thread_data[THREAD_NUM];
+  for(int t=0;t<THREAD_NUM;t++){
+
+    printf("In main: creating thread %ld\n", t);
+    thread_data[t] = t;
+    int rc = pthread_create(&threads[t], NULL, thread, &thread_data[t]);
+    if (rc){
+
+      printf("ERROR; return code from pthread_create() is %d\n", rc);
+      exit(-1);
+    }
+  }
+
+  for(int i = 0; i < THREAD_NUM; i++) 
+    pthread_join(threads[i], NULL);
+
+  /* Last thing that main() should do */
+  pthread_exit(NULL);
+}
